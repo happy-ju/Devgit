37e58c511c69b9de9ce0d9348f50d56e8d3a6185
ju <ju@jus-MacBook-Pro.jlmg.ae>
2021-04-26 12:34:11 +0400

    branch4

--- src/test/java/pckg/Test4.java
+++ src/test/java/pckg/Test4.java
@@ -4,5 +4,8 @@ public class Test4 {
     public static void sayHello(String name){
         System.out.println("Hello" + name);
         System.out.println("hi");
+        System.out.println("Bye");
     }
+
+
 }
