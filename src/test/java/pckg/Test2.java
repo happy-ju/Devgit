bcf7aa9d716d302c02c97ab7bcae8ce3c52b9903
ju <ju@jus-MacBook-Pro.jlmg.ae>
2021-04-26 10:07:54 +0400

    Add h1 method

--- src/test/java/pckg/Test2.java
+++ src/test/java/pckg/Test2.java
@@ -3,5 +3,10 @@ package pckg;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Hello World #2");
+        System.out.println("Bye");
+    }
+
+    public static void hi(String name) {
+        System.out.println("hello" + name);
     }
 }
