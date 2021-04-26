77eb3947913cfb163be4ee81ca56ee5b3bc621b6
ju <ju@jus-MacBook-Pro.jlmg.ae>
2021-04-26 11:39:47 +0400

    new file with Mark

--- src/test/java/pckg/Test3.java
+++ src/test/java/pckg/Test3.java
@@ -3,6 +3,7 @@ package pckg;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Marki hi");
     }
         public static void printNum ( int num){
             System.out.println("your number is  " + num);
